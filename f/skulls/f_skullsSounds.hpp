67d455c6442dc4da6c2dba913b580e745d8fcbdf
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2022-12-09 00:27:34 +0000

    Add special effects

---
+++ f/skulls/f_skullsSounds.hpp
@@ -0,0 +1,9 @@
+// FA3 - Skulls module - sounds config included in description.ext
+class f_gruntBirthdayParty
+{
+	// how the sound is referred to in the editor (e.g. trigger effects)
+	name = "Grunt Birthday Party";
+	// filename, volume, pitch, distance (optional)
+	sound[] = { "f\skulls\gruntBirthdayParty.ogg", 1, 1, 100 };
+	titles[] = {};
+};
